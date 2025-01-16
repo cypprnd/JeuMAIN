8795e6cd9b8fd21efdc8b5ba56ea74df5113e831
francisjg23 <147619750+francisjg23@users.noreply.github.com>
2025-01-16 11:31:26 +0100

    directory

--- Polytech.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-class Polytech {
-public:
-    virtual void afficher() = 0;  // Fonction virtuelle pure pour afficher des informations
-};
