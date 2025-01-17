404def1f2d1269c73e2a88f706555e63bcd0aa49
francisjg23 <147619750+francisjg23@users.noreply.github.com>
2025-01-17 17:46:20 +0100

    fin

--- test/TestCase.cc
+++ test/TestCase.cc
@@ -14,14 +14,14 @@ TEST_SUITE("Jeu MAin"){
 
 TEST_CASE("1: Stats"){
   Stats stats;
-  CHECK(stats.energie == 100);
-  CHECK(stats.reputation == 50);
+  CHECK(stats.energie == 80);
+  CHECK(stats.reputation == 35);
   CHECK(stats.revision == 0);
   CHECK(stats.note == -1);
 
   stats += 30;
   CHECK(stats.energie == 100);
-  CHECK(stats.reputation == 80);
+  CHECK(stats.reputation == 65);
   CHECK(stats.revision == 30);
   CHECK(stats.note == -1);
 
@@ -35,13 +35,13 @@ TEST_CASE("2: Personnage"){
 
   cout << yves << std::endl;
   CHECK(yves.getNom() == "Yves");
-  CHECK(yves.getRole() == "Eleve");
+  CHECK(yves.getRole() == "Élève");
   yves.parler(stats);
   CHECK(yves.get_rencontre() == 2);
 
   cout << prof << std::endl;
   CHECK(prof.getNom() == "Michel");
-  CHECK(yves.getRole() == "Professeur");
+  CHECK(prof.getRole() == "Professeur");
   prof.parler(stats);
   CHECK(yves.get_rencontre() == 2);
 
@@ -59,20 +59,20 @@ TEST_CASE("3: Evenement" ) // Changer par les bonnes valeurs
   Vacances vac;
 
   revision.executer(stats);
-  CHECK(stats.energie == 95);
+  CHECK(stats.energie == 75);
   CHECK(stats.revision == 10);
 
   sortir.executer(stats);
-  CHECK(stats.energie == 90);
-  CHECK(stats.reputation == 60);
+  CHECK(stats.energie == 70);
+  CHECK(stats.reputation == 45);
 
   reposer.executer(stats);
-  CHECK(stats.energie == 95);
+  CHECK(stats.energie == 75);
 
   rencontreYves.executer(stats);
-  CHECK(stats.energie == 100);
-  CHECK(stats.revision == 17);
-  CHECK(stats.reputation == 67);
+  CHECK(stats.energie == 83);
+  CHECK(stats.revision == 18);
+  CHECK(stats.reputation == 53);
 
   vac.executer(stats);
   CHECK(stats.energie == 100);
