3c84e917d618dc95dee9b191f772f3a0bbf1e410
francisjg23 <147619750+francisjg23@users.noreply.github.com>
2025-01-17 17:34:43 +0100

    fin

--- stats.hpp
+++ stats.hpp
@@ -7,12 +7,14 @@ struct Stats {
     int energie;  // Énergie restante (0 à 100)
     int reputation; // Réputation (0 à 100)
     int revision;
+    int somme_note;
+    int nb_note;
 
     int niveau;
     std::string difficulte;
 
-    Stats() : note(-1), energie(100), reputation(50), revision(0) {
-        niveau = energie + revision - 20;
+    Stats() : note(-1), energie(80), reputation(35), revision(0),somme_note(0), nb_note(0) {
+        niveau = energie + revision - 10;
         if(niveau >= 150) difficulte = "facile";
         else if(niveau < 150 && niveau > 75) difficulte = "moyen";
         else difficulte = "difficile";
@@ -26,6 +28,11 @@ struct Stats {
         std::cout << "------------------------------" << std::endl;
     }
 
+    void maj_note(){
+        if(nb_note != 0) note = somme_note/nb_note;
+        else note = -1;
+    }
+
     void maj_dif(){
         niveau = energie + revision - 20;
         if(niveau >= 150) difficulte = "facile";
@@ -35,9 +42,14 @@ struct Stats {
 
     void reset(){
         note = -1;
-        energie = 100;
+        nb_note = 0;
+        somme_note = 0;
+        if(energie < 80) energie = 80;
+        revision -= 15;
+        if(revision < 0) revision = 0;
+        reputation -= 30;
+        if(reputation < 0) revision = 0;
     }
-
 };
 
 Stats& operator+=(Stats& stats, int val) {
